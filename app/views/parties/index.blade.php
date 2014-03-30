599d859032b6720cb265110fd5c857ee0b76361e
John <php.john.c@gmail.com>
2014-03-30 21:24:16 +0530

    added the parties list

--- app/views/parties/index.blade.php
+++ app/views/parties/index.blade.php
@@ -4,7 +4,10 @@
 @section('content')
 	<div class="panel panel-default">
 		<div class="panel-heading"><h3>Parties</h3></div>
-		<div class="panel-body"></div>
+		<div class="panel-body">
+			<table id="productList"></table>
+			<div id="productPager"></div>
+		</div>
 		<div class="panel-footer">
 			<button class="btn btn-inverse" data-toggle="modal" data-target="#addParty">New Party</button>
 		</div>
@@ -37,4 +40,52 @@
 
 <!-- Page based Scripts -->
 @section('script')
+<script>
+var panelWidth = jQuery(".panel").width()-30;
+jQuery("#productList").jqGrid({ 
+	datatype: "local",
+	height: 250,
+	width: panelWidth,
+	colNames:['Party Code','Party Name', 'Country', 'City', 'Address', 'Fax Number', 'Telephone Number', 'Postal Code', 'Contact Name', 'Credit Limit', 'Payment Terms', 'Business Hour', 'Party service Level', 'Order Priority', 'Services Provided', 'Created Date'],
+	colModel:[
+		{name:'username'},
+		{name:'fname'},
+		{name:'lname'},
+		{name:'email'},
+		{name:'warehouse'},
+		{name:'status'},
+		{name:'exp_on'},
+		{name:'created_at'},
+		{name:'username'},
+		{name:'fname'},
+		{name:'lname'},
+		{name:'email'},
+		{name:'warehouse'},
+		{name:'status'},
+		{name:'exp_on'},
+		{name:'created_at'}
+	], 
+	rowNum:10, 
+	rowList:[10,20,30], 
+	pager: '#productPager', 
+	viewrecords: true, 
+	sortorder: "desc", 
+	loadonce: true
+});
+var mydata = [
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"},
+	{username:"Loram",fname:"Loram",lname:"Loram",email:"Loram",warehouse:"Loram",status:"Loram",exp_on:"Loram",created_at:"Loram"}
+];
+	for(var i=0;i<=mydata.length;i++)
+		$("#productList").addRowData(i+1,mydata[i]);
+
+</script>
 @stop
--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -4,54 +4,53 @@
 	<div class="panel panel-default">
 		<div class="panel-heading">Product List</div>
 		<div class="panel-body">
-		
 			<table id="productList"></table>
 			<div id="productPager"></div>
-		
 		</div>
 	</div>
 @stop
 
 @section('script')
 <script>
+var panelWidth = jQuery(".panel").width()-30;
 jQuery("#productList").jqGrid({ 
 	datatype: "local",
 	height: 250,
-	width: 950,
-	colNames:['Username','First Name', 'Last Name', 'Email ID', 'Warehouse Name', 'Status', 'Expiry Date', 'Created Date'],
+	width: panelWidth,
+	colNames:['Client Code','Product  Code', 'Product Name', 'Product description', 'Product Category', 'UOM', 'Product Dimensions', 'Serial Number', 'Expiry date', 'Storage Form', 'Location Area','Created Date'],
 	colModel:[
-		{name:'username'},
-		{name:'fname'},
-		{name:'lname'},
-		{name:'email'},
-		{name:'warehouse'},
-		{name:'status'},
-		{name:'exp_on'},
+		{name:'client_code'},
+		{name:'product_code'},
+		{name:'product_name'},
+		{name:'product_desc'},
+		{name:'product_cat'},
+		{name:'uom'},
+		{name:'product_dimensions'},
+		{name:'serial_number'},
+		{name:'expiry_date'},
+		{name:'storage_form'},
+		{name:'location_area'},
 		{name:'created_at'}
 	], 
 	rowNum:10, 
-	rowList:[10,20,30], 
+	rowList:[10,20,30],
 	pager: '#productPager', 
 	viewrecords: true, 
 	sortorder: "desc", 
 	loadonce: true
 });
 var mydata = [
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
-	{username:"shahul",fname:"Shahul",lname:"Hameed",email:"shameed.pro@gmail.com",warehouse:"Loram Ipsam",status:"Active",exp_on:"2015-04-04",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	{client_code:"1005",product_code:"10456",product_name:"HP Pavilion 1000-1B02AU Laptop",product_desc:"14 inch Screen, AMD Dual Core Processor, 2 GB Memory, 320 GB HDD, Free DOS",product_cat:"Laptop",uom:"",product_dimensions:"1600 x 900",serial_number:"1000056",expiry_date:"2015-04-04",storage_form:"",location_area:"US",created_at:"2014-03-04"},
+	
 ];
 	for(var i=0;i<=mydata.length;i++)
 		$("#productList").addRowData(i+1,mydata[i]);
