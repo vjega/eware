c4c7abcb8b4383f4fc059a805592b186845f79cf
Jegatheesan Veeramalai <jega@jega.in>
2014-04-06 22:34:52 +0530

    more on site

--- app/controllers/SiteAPIController.php
+++ app/controllers/SiteAPIController.php
@@ -25,23 +25,34 @@ class SiteAPIController extends \BaseController {
     {
         $postData =  Input::get("data");
         $postObj = json_decode($postData);
-        $company = new Company;
-        $company->name          = $postObj->name;
-        $company->country_code  = $postObj->country;
-        $company->city          = $postObj->city;
-        $company->address       = $postObj->address;
-        $company->fax_no        = $postObj->fax_no;
-        $company->tel_no        = $postObj->telno;
-        $company->postal_code   = $postObj->postal_code;
-        $company->contact_name  = $postObj->cont_name;
-        $company->credit_limit  = $postObj->credit_limit;
-        $company->payment_terms = $postObj->paymnt_terms;
-        $company->biz_hour      = $postObj->opening_hours;
-        $company->party_service_level = $postObj->order_priority;
-        $company->order_priority      = $postObj->order_priority;
-        $company->services_provided   = $postObj->service_provided;
-        $company->save(); 
-        return $company->id;
+        $site = new Site;
+        $site->company_code = $postObj->company_code;
+        $site->name         = $postObj->name;
+        $site->country          = $postObj->country;
+        $site->region       = $postObj->region;
+        $site->city        = $postObj->city;
+        $site->currency        = $postObj->currency;
+        $site->addrs1   = $postObj->addrs1;
+        $site->addrs2  = $postObj->addrs2;
+        $site->addrs3  = $postObj->addrs3;
+        $site->email = $postObj->email;
+        $site->website      = $postObj->website;
+        $site->tel_no1 = $postObj->tel_no1;
+        $site->tel_no2      = $postObj->tel_no2;
+        $site->fax_no   = $postObj->fax_no;
+        $site->postal_code   = $postObj->postal_code;
+        $site->biz_type   = $postObj->biz_type;
+        $site->biz_hours   = $postObj->biz_hours;
+        $site->credit_limit   = $postObj->credit_limit;
+        $site->priority   = $postObj->priority;
+        $site->zonal_level   = $postObj->zonal_level;
+        $site->vendor_file = '';
+        $site->customer_file = '';
+        $site->support_source_file = '';
+        $site->threepl_file = '';
+        $site->save(); 
+        return $site->id;
+
     }
 
     public function show($id)
@@ -69,7 +80,6 @@ class SiteAPIController extends \BaseController {
         $company->party_service_level = $postObj->order_priority;
         $company->order_priority      = $postObj->order_priority;
         $company->services_provided   = $postObj->service_provided;
-        $company->save(); 
         return $company->id;
     }
 
