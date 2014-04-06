4001fbe4ea3715322c0c34ae35ac9a32c0bb1981
Jegatheesan Veeramalai <jega@jega.in>
2014-04-06 20:02:44 +0530

    uom model added

---
+++ app/controllers/UomAPIController.php
@@ -0,0 +1,82 @@
+<?php
+
+class UomAPIController extends \BaseController {
+
+    //public $restful = true;
+    /**
+     * Display a listing of companies
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $uom_arr = UOM::all()->toArray();
+        $uom['page'] = 1;
+        $uom['total'] = 1;
+        $uom['records'] = count($uom_arr);
+        $uom['rows'] = $uom_arr;
+        foreach ($uom['rows'] as $key=>$value) {
+            $uom['rows'][$key]['select'] = "<input type='checkbox' id='{$value['id']}' class='uombox' />";
+        }
+        return $uom;
+    }
+
+    public function store()
+    {
+        $postData =  Input::get("data");
+        $postObj = json_decode($postData);
+        $company = new Company;
+        $company->name          = $postObj->name;
+        $company->country_code  = $postObj->country;
+        $company->city          = $postObj->city;
+        $company->address       = $postObj->address;
+        $company->fax_no        = $postObj->fax_no;
+        $company->tel_no        = $postObj->telno;
+        $company->postal_code   = $postObj->postal_code;
+        $company->contact_name  = $postObj->cont_name;
+        $company->credit_limit  = $postObj->credit_limit;
+        $company->payment_terms = $postObj->paymnt_terms;
+        $company->biz_hour      = $postObj->opening_hours;
+        $company->party_service_level = $postObj->order_priority;
+        $company->order_priority      = $postObj->order_priority;
+        $company->services_provided   = $postObj->service_provided;
+        $company->save(); 
+        return $company->id;
+    }
+
+    public function show($id)
+    {
+        $company = Company::find($id);
+        return $company;
+    }
+
+    public function update($id)
+    {
+        $company = Company::find($id);
+        $postData =  Input::get("data");
+        $postObj = json_decode($postData);
+        $company->name          = $postObj->name;
+        $company->country_code  = $postObj->country;
+        $company->city          = $postObj->city;
+        $company->address       = $postObj->address;
+        $company->fax_no        = $postObj->fax_no;
+        $company->tel_no        = $postObj->telno;
+        $company->postal_code   = $postObj->postal_code;
+        $company->contact_name  = $postObj->cont_name;
+        $company->credit_limit  = $postObj->credit_limit;
+        $company->payment_terms = $postObj->paymnt_terms;
+        $company->biz_hour      = $postObj->opening_hours;
+        $company->party_service_level = $postObj->order_priority;
+        $company->order_priority      = $postObj->order_priority;
+        $company->services_provided   = $postObj->service_provided;
+        $company->save(); 
+        return $company->id;
+    }
+
+    public function destroy($id)
+    {
+        $ids = explode(',',$id);
+        Company::destroy($ids);
+        return "true";
+    }
+}
---
+++ app/controllers/UomController.php
@@ -0,0 +1,17 @@
+<?php
+
+class UomController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return View::make('uoms.index');
+	}
+
+	
+
+}
