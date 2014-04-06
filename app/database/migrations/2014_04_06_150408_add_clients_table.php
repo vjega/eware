c98640b8ebb158673f64efe4e8ee2c879639500d
Jegatheesan Veeramalai <jega@jega.in>
2014-04-07 01:37:49 +0530

    more on sites

--- app/database/migrations/2014_04_06_150408_add_clients_table.php
+++ app/database/migrations/2014_04_06_150408_add_clients_table.php
@@ -13,21 +13,21 @@ class AddClientsTable extends Migration {
 	public function up()
 	{
 		Schema::table('clients', function(Blueprint $table) {
-			$table->string('client_code', 10);	
-			$table->string('name', 300);	
-			$table->string('country_code', 10);	
-			$table->string('city', 50);	
-			$table->string('address', 500);	
-			$table->string('fax_no', 20);	
-			$table->string('tel_no', 20);	
-			$table->string('postal_code', 20);	
-			$table->string('contact_name', 300);	
-			$table->decimal('credit_limit', 10, 2);	
-			$table->string('payment_terms', 10);	
-			$table->string('business_hour', 10);	
-			$table->string('party_service_level', 100);	
-			$table->string('order_priority', 10);	
-			$table->string('services_provided', 100);
+			$table->string('client_code', 10)->default('');	
+			$table->string('name', 300)->default('');	
+			$table->string('country_code', 10)->default('');	
+			$table->string('city', 50)->default('');	
+			$table->string('address', 500)->default('');	
+			$table->string('fax_no', 20)->default('');	
+			$table->string('tel_no', 20)->default('');	
+			$table->string('postal_code', 20)->default('');	
+			$table->string('contact_name', 300)->default('');	
+			$table->decimal('credit_limit', 10, 2)->default('0.00');	
+			$table->string('payment_terms', 10)->default('');	
+			$table->string('business_hour', 10)->default('');	
+			$table->string('party_service_level', 100)->default('');	
+			$table->string('order_priority', 10)->default('');	
+			$table->string('services_provided', 100)->default('');
 		});
 	}
 
