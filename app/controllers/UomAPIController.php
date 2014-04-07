d6392eedd3ba8c058d439b7ac04930ae15552d77
Jegatheesan Veeramalai <jega@jega.in>
2014-04-08 00:20:22 +0530

    uom added

--- app/controllers/UomAPIController.php
+++ app/controllers/UomAPIController.php
@@ -25,58 +25,34 @@ class UomAPIController extends \BaseController {
     {
         $postData =  Input::get("data");
         $postObj = json_decode($postData);
-        $company = new Company;
-        $company->name          = $postObj->name;
-        $company->country_code  = $postObj->country;
-        $company->city          = $postObj->city;
-        $company->address       = $postObj->address;
-        $company->fax_no        = $postObj->fax_no;
-        $company->tel_no        = $postObj->telno;
-        $company->postal_code   = $postObj->postal_code;
-        $company->contact_name  = $postObj->cont_name;
-        $company->credit_limit  = $postObj->credit_limit;
-        $company->payment_terms = $postObj->paymnt_terms;
-        $company->biz_hour      = $postObj->opening_hours;
-        $company->party_service_level = $postObj->order_priority;
-        $company->order_priority      = $postObj->order_priority;
-        $company->services_provided   = $postObj->service_provided;
-        $company->save(); 
-        return $company->id;
+        $uom = new Uom;
+        $uom->uom_code          = $postObj->uom_code;
+        $uom->description  = $postObj->description;
+        $uom->save(); 
+        return $uom->id;
     }
 
     public function show($id)
     {
-        $company = Company::find($id);
-        return $company;
+        $uom = Uom::find($id);
+        return $uom;
     }
 
     public function update($id)
     {
-        $company = Company::find($id);
+        $uom      = Uom::find($id);
         $postData =  Input::get("data");
-        $postObj = json_decode($postData);
-        $company->name          = $postObj->name;
-        $company->country_code  = $postObj->country;
-        $company->city          = $postObj->city;
-        $company->address       = $postObj->address;
-        $company->fax_no        = $postObj->fax_no;
-        $company->tel_no        = $postObj->telno;
-        $company->postal_code   = $postObj->postal_code;
-        $company->contact_name  = $postObj->cont_name;
-        $company->credit_limit  = $postObj->credit_limit;
-        $company->payment_terms = $postObj->paymnt_terms;
-        $company->biz_hour      = $postObj->opening_hours;
-        $company->party_service_level = $postObj->order_priority;
-        $company->order_priority      = $postObj->order_priority;
-        $company->services_provided   = $postObj->service_provided;
-        $company->save(); 
-        return $company->id;
+        $postObj  = json_decode($postData);
+        $uom->uom_code     = $postObj->uom_code;
+        $uom->description  = $postObj->description;
+        $uom->save(); 
+        return $uom->id;
     }
 
     public function destroy($id)
     {
         $ids = explode(',',$id);
-        Company::destroy($ids);
+        Uom::destroy($ids);
         return "true";
     }
 }
